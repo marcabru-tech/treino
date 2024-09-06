ddc5d59c90459a49ee361c7b8d4ff3e49ec20cab
marcabru-tech <dommarcabrugmail.com>
2024-09-06 16:34:16 -0300

    Mais duas linhas de código

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 boolean casado = true;
-boolean filhos = true;
+boolean tem_filhos = true;
+boolean bens = true;
         System.out.println(casado);
 
         }
